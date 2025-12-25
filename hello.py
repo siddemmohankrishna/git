149847d6bc00ff5e22adee62218cc7ad2b1eafca
Siddem Mohan Krishna <siddemmohankrishna@gmail.com>
2025-12-25 19:21:38 +0530

    give a commit

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 <<<<<<< HEAD
 # This is python file for hello
 import pandas as pd
+import sklearn
 import numpy as np
 =======
 print("Hello world")
 print("End")
 >>>>>>> python
+print("Thanks")
+print("End")
